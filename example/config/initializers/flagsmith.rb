5740dd0a61285509ed10bb9974b2886d2322ad11
Matthew Elwell <matthewe@solidstategroup.com>
2022-06-08 16:34:43 +0100

    Rationalise flags models to a single file

--- example/config/initializers/flagsmith.rb
+++ example/config/initializers/flagsmith.rb
@@ -2,6 +2,6 @@ $flagsmith = Flagsmith::Client.new(
   enable_local_evaluation: true,
   environment_refresh_interval_seconds: 60,
   default_flag_handler: lambda { |feature_name|
-    Flagsmith::DefaultFlag.new(enabled: false, value: {}.to_json)
+    Flagsmith::Flags::DefaultFlag.new(enabled: false, value: {}.to_json)
   }
 )
