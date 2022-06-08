934e79340e58845f67df60c247bfa7e7318cb0b0
Matthew Elwell <matthewe@solidstategroup.com>
2022-06-08 15:56:33 +0100

    Update example code

--- example/config/initializers/flagsmith.rb
+++ example/config/initializers/flagsmith.rb
@@ -2,8 +2,6 @@ $flagsmith = Flagsmith::Client.new(
   enable_local_evaluation: true,
   environment_refresh_interval_seconds: 60,
   default_flag_handler: lambda { |feature_name|
-    Flagsmith::Flag.new(
-      feature_name: feature_name, enabled: false, value: {}.to_json, feature_id: nil
-    )
+    Flagsmith::DefaultFlag.new(enabled: false, value: {}.to_json)
   }
 )
