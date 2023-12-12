64607f98e0cfcc95e9780f9ff8203e62966bb74f
Zach Aysan <zachaysan@gmail.com>
2023-12-12 15:33:39 +0000

    Add offline handler

---
+++ lib/flagsmith/sdk/offline_handlers.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Flagsmith
+  module OfflineHandlers
+    class LocalFileHandler
+
+      attr_reader :environment
+
+      def initialize(environment_document_path)
+        environment_file = File.open(environment_document_path)
+
+        data = JSON.parse(environment_file.read, symbolize_names: true)
+        @environment = Flagsmith::Engine::Environment.build(data)
+      end
+
+    end
+  end
+end
