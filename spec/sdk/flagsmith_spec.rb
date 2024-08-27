6a9c4b896fc6033285e109e7f2c3c8bd8fcc9794
Kim Gustyr <kim.gustyr@flagsmith.com>
2024-08-27 18:04:34 +0100

    feat: Support transient identities and traits - Add optional `transient` argument to `get_identity_flags` - Add support for trait configuration values with optional `transient` key - Remove unneeded default argument values from private methods - Remove unneeded comments from the spec

--- spec/sdk/flagsmith_spec.rb
+++ spec/sdk/flagsmith_spec.rb
@@ -62,39 +62,6 @@ RSpec.describe Flagsmith do
     end
   end
 
-  # describe '#set_trait' do
-  #   let(:trait_key) { 'foo' }
-  #   let(:trait_value) { 'bar' }
-  #   let(:post_body) do
-  #     {
-  #       identity: { identifier: user_id },
-  #       trait_key: Flagsmith::Flags::Collection.normalize_key(trait_key),
-  #       trait_value: trait_value
-  #     }.to_json
-  #   end
-  #
-  #   it 'sets a trait for a given user' do
-  #     trait_response = OpenStruct.new(body: {})
-  #     expect(mock_api_client).to receive(:post).with('traits/', post_body).and_return(trait_response)
-  #     subject.set_trait user_id, trait_key, trait_value
-  #   end
-  #
-  #   it 'errors if user_id.nil?' do
-  #     expect { subject.set_trait nil, trait_key, trait_value }.to raise_error(StandardError)
-  #   end
-  # end
-
-  # describe '#get_traits' do
-  #   it 'returns hash of traits for a given user' do
-  #     traits = subject.get_traits(user_id)
-  #     expect(traits['roles']).to eq(%w[admin staff].to_json)
-  #     expect(traits.length).to eq(2)
-  #   end
-  #   it 'returns {} for user_id.nil?' do
-  #     expect(subject.get_traits(nil)).to eq({})
-  #   end
-  # end
-
   describe '#normalize_key' do
     it 'returns an empty string given nil' do
       expect(Flagsmith::Flags::Collection.normalize_key(nil)).to eq('')
@@ -120,6 +87,43 @@ RSpec.describe Flagsmith do
       polling_manager = Flagsmith::EnvironmentDataPollingManager.new(subject, 60, 10)
       subject.update_environment()
       expect(subject.get_identity_segments("identifier", {"age": 39}).length).to eq(1)
+    end
+  end
+
+  describe '#get_identity_flags' do
+    context 'with transient identity' do
+      before {
+        transient_identities_response = OpenStruct.new(body: { **identities_response.body } )
+        transient_identities_response.body[:traits] = identities_response.body[:traits].map{ |api_trait| { **api_trait, transient: true } }
+        allow(mock_api_client).to receive(:post).with(
+          'identities/', { identifier: user_id, transient: true, traits: [] }.to_json
+        ).and_return(transient_identities_response)    
+      }
+
+      it 'sends expected request' do
+        flags = subject.get_identity_flags(user_id, true)
+        expect(flags.feature_enabled?(:feature_one)).to eq(false)
+        expect(flags.feature_enabled?('feature_two')).to eq(true)
+        expect(flags.feature_enabled?(:feature_three)).to eq(true)
+      end
+    end
+
+    context 'with transient trait' do 
+      before {
+        transient_trait_data = { trait_key: "age", trait_value: 42, transient: true }
+        transient_traits_response = OpenStruct.new(body: { **identities_response.body } )
+        transient_traits_response.body[:traits] = transient_traits_response.body[:traits] + [transient_trait_data]
+        allow(mock_api_client).to receive(:post).with(
+          'identities/', { identifier: user_id, transient: false, traits: [transient_trait_data] }.to_json
+        ).and_return(transient_traits_response)    
+      }
+
+      it 'sends expected request' do
+        flags = subject.get_identity_flags(user_id, age: { value: 42, transient: true })
+        expect(flags.feature_enabled?(:feature_one)).to eq(false)
+        expect(flags.feature_enabled?('feature_two')).to eq(true)
+        expect(flags.feature_enabled?(:feature_three)).to eq(true)
+      end
     end
   end
 end
